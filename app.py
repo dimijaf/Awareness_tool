21a0d8c309e359124262a784f67fa87d630e3945
dimijaf <94046049+dimijaf@users.noreply.github.com>
2026-02-09 11:35:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ for i, sheet_name in enumerate(SHEETS.keys()):
                 height=1000, 
                 use_container_width=True,
                 column_config={
-                    col: st.column_config.Column(width="medium") 
+                    col: st.column_config.Column(width="small") 
                     for col in df_t.columns
                 }
             )
