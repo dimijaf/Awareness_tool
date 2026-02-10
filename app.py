3266a9784c55d92d7e230df72bc8fed19b6cdcc9
dimijaf <94046049+dimijaf@users.noreply.github.com>
2026-02-10 13:47:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,10 @@ for i, sheet_name in enumerate(SHEETS.keys()):
                         (realtime['QuestionnaireDate'].str.contains(date_str))
                     ]
                     cum_sum = len(historical_data)
-                    date_avg[col] = round(cum_sum / days_up_to_date, 3) if days_up_to_date > 0 else 0
+                    if days_up_to_date > 0:
+                        date_avg[col] = round(cum_sum / days_up_to_date, 3)
+                    else:
+                        date_avg[col] = 0
 
 
                
