e25d6b502cee2952e3413984ae51b6d37d9db751
dimijaf <94046049+dimijaf@users.noreply.github.com>
2026-02-16 17:52:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -143,8 +143,8 @@ for i, sheet_name in enumerate(["Report", "RealTime", "Questions", "Graph"]):
                 height=1000,
                 use_container_width=True,
                 column_config={
-                    col: st.column_config.Column(width="10") 
-                    for col in styled_df.columns
+                    col: st.column_config.Column(width="small") 
+                    for col in df_t.columns
                 }
             )
             st.session_state["Report_df_t"] = df_t
